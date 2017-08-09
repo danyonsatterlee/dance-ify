ac21cbfc59502635e04fd8369fb72ff4cadf0ecc
danyonsatterlee <danyon.satterlee@gmail.com>
2017-08-09 14:31:16 -0600

    more style

--- src/components/playlist.js
+++ src/components/playlist.js
@@ -10,10 +10,10 @@ class Playlist extends React.Component{
        console.log(this.props.track.uri)
         return(
 <div className="col-md-12 playlist-bottom " data-wow-offset="70" data-wow-iteration="1">
-     <div className="col-md-2 col-md-offset-3 ">
+     <div className="col-md-2 col-md-offset-2 ">
     <img className="playlist-img" src={this.props.playlist.images[0].url}/> 
     </div>
-    <div className="col-md-7">
+    <div className="col-md-8">
     <h4 className="artist-name">{this.props.playlist.name}</h4>
     <button className="add-to-playlist-2"onClick={()=>{this.props.handleSong(this.props.playlist.href, this.props.track.uri, this.props.accessToken)}}>Add Song to Playlist  <FontAwesome
        
